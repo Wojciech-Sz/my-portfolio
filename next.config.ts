ae8712619f175a3881ebed4539da12fe1743696e
WojciechSz <w.szczygielski00@gmail.com>
2025-01-27 12:15:54 +0100

    Basic config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  transpilePackages: ["three"],
 };
 
 export default nextConfig;
