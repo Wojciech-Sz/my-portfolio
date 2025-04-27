290208baab0f594b5b1af422ed6871e76f4fd49e
WojciechSz <w.szczygielski00@gmail.com>
2025-04-27 16:23:41 +0200

    SEO delete

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import PricingSEO from "./PricingSEO";
 import PricingWeb from "./PricingWeb";
 
 const Services = () => {
@@ -10,7 +9,6 @@ const Services = () => {
         My <span className="text-cosmic-blue_gradient">Services</span>
       </h2>
       <PricingWeb />
-      <PricingSEO />
     </section>
   );
 };
